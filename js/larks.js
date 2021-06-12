e95f902ac8e26f3c67de52d942f3bad3fc565d68
SATHYAPRASAD KOTTAPALLI, KRISHNA <KRISHNA.SATHYAPRASADKOTTAPALLI@globalfoundries.com>
2021-06-12 17:11:15 +0530

    lots of changes in website

--- js/larks.js
+++ js/larks.js
@@ -1,13 +1,55 @@
 $(document).ready(function () {
-    $("[data-trigger]").on("click", function(){
-        var trigger_id =  $(this).attr('data-trigger');
+    $("[data-trigger]").on("click", function () {
+        var trigger_id = $(this).attr('data-trigger');
         $(trigger_id).toggleClass("show");
         $('body').toggleClass("offcanvas-active");
     });
-    
+
     // close button 
-    $(".btn-close").click(function(e){
+    $(".btn-close").click(function (e) {
         $(".navbar-collapse").removeClass("show");
         $("body").removeClass("offcanvas-active");
+    });
+
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+
+    $('.customer-testimonials').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
     });
 });
